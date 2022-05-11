d82f3e9b62902f657aae284b03bc05012a42d318
vquine <01010110.v@gmail.com>
2022-05-10 21:52:35 -0400

    Container component meant to house a blog selection menu and post content

---
+++ src/containers/Blog.js
@@ -0,0 +1,19 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import * as React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-MATERIAL UI IMPORTS
+import Grid from '@material-ui/core/Grid'
+import { Typography } from '@material-ui/core'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-JSON DATA OBJECTS
+import Menu from '../components/Menu'
+import blogsData from '../assets/data/blogsData'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+
+export default function Blog() {
+
+  return (<>
+    <Menu />
+  </>)
+
+}
