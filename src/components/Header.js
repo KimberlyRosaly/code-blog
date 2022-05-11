caf74f5d0dfa1df2356969cc9bb12478b846072d
KimberlyRosaly <01010110.v@gmail.com>
2022-05-11 02:05:33 -0400

    remove paper from logo part of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { Card } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,26 +19,33 @@ export default function Header() {
 
   return (
     <div className={classes.root}>
-      <Grid container justifyContent='center' alignItems='center' spacing={1}>
-        <Grid 
-          item 
-          xs={5}>
-          <Paper className={classes.paper}>
-            <img 
-            style={{maxWidth:"200px"}}
-            src={`${'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Pencil_BLACK-01.svg/1200px-Pencil_BLACK-01.svg.png'}`} />
-          </Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-          code.blog
-          </Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-          by kimberly rosaly
-          </Paper>
-        </Grid>
+
+      <Grid 
+        container 
+        justifyContent='center' 
+        alignItems='center' 
+        spacing={1}
+        >
+          <Grid item xs={5}>
+            <div className={classes.paper}>
+              <img 
+              style={{maxWidth:"200px"}}
+              src={`${'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Pencil_BLACK-01.svg/1200px-Pencil_BLACK-01.svg.png'}`} />
+            </div>
+          </Grid>
+
+            <Grid item xs>
+              <Paper className={classes.paper}>
+              code.blog
+              </Paper>
+          </Grid>
+
+          <Grid item xs>
+            <Paper className={classes.paper}>
+            by kimberly rosaly
+            </Paper>
+          </Grid>
+
       </Grid>
     </div>
   );
