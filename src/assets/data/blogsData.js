09931d37a0d0a3748324d9867fbf70fe95cccc5a
KimberlyRosaly <01010110.v@gmail.com>
2022-05-11 02:08:03 -0400

    add more dummy data to iterate through and render on page

---
+++ src/assets/data/blogsData.js
@@ -0,0 +1,84 @@
+const blogsData = [
+    {
+        id: 1,
+        date: "June 50th, 2525",
+        time: "2:65PM",
+        title: "Blog Test No. 1 TITLE",
+        image: "https://www.dropbox.com/s/ib8gjbkfc48ph76/Css.png?raw=1",
+        content: <div>MESSAGE BODY lives here</div>
+    },
+
+    {
+        id: 2,
+        date: "March 31st, 1999",
+        time: "4400 Hours",
+        title: "No. 2, Test Blog, TiTlE",
+        image: "https://www.dropbox.com/s/hgm59uqck7f1mqz/Reactshell.png?raw=1",
+        content: <div>abdefg<h1>kjfgpwjfp;lwfr</h1>hijklmnopqrstuvwxyz</div>
+    },
+
+    {
+        id: 3,
+        date: "00-00-0000",
+        time: "00:00XX",
+        title: "ASDFJK LZXCVBNM QWERTYU IOP",
+        image: "https://www.dropbox.com/s/2qdiu0281n5pt0t/Redux.png?raw=1",
+        content: <div>this is a test <h1> this is <hr /> another test </h1> LLKAJOIER OINFEWOIJFJ OKJVWEOIETWOKJ OINWFOIJEFW  OJEWFWOINF DFSKLNF  FDLKNFLKj FLKNSDFLknF FKLJNFSDLkjFSD FOKJFDlkFSDkljFSD jfsdlkjfsdljkfsljfsljkfljfsljkfs LKJDSFLKJoijF SFKLNk sflkjlfjsflkj lskjlsfjsfiewuriwuoksjoisjgflsjlsfjl LKnfdlknflkjsdlkjsflkjsflkjsflkjvknsdvlk lkjfslkjfslj sfl flkjslfkjlkjfs l  fwlkjfojwf oijlskjfg amn osij ;slakheogfhegl;heg ;ljhagl;kjhdfgkjhg jahg ;kgh h jhgajghksdjghuiwhytoiwhkjng</div>
+        
+    },
+
+    {
+        id: 4,
+        date: "00-00-0000",
+        time: "00:00XX",
+        title: <div>Flatiron Graduation<br />&<br />Pigs Fly</div>,
+        image: "https://www.dropbox.com/s/u0a5lxj96oue7fa/IMG_1728.jpg?raw=1",
+        content: <div>
+            February 15th, 2022 arrives and I never smiled more. Getting the news of graduation was the most important moment of my life. 
+            </div>
+        
+    },
+    {
+        id: 5,
+        date: "00-00-0000",
+        time: "00:00XX",
+        title: <div>Flatiron Graduation<br />&<br />Pigs Fly</div>,
+        image: "https://www.dropbox.com/s/hgm59uqck7f1mqz/Reactshell.png?raw=1",
+        content: <div>
+            February 15th, 2022 arrives and I never smiled more. Getting the news of graduation was the most important moment of my life. 
+            </div>
+        
+    },
+    {
+        id: 6,
+        date: "00-00-0000",
+        time: "00:00XX",
+        title: <div>THIS THAT These Those Noun Adverb Verb</div>,
+        image: "https://www.dropbox.com/s/u0a5lxj96oue7fa/IMG_1728.jpg?raw=1",
+        content: <div>
+            February 15th, 2022 arrives and I never smiled more. Getting the news of graduation was the most important moment of my life. 
+            </div>
+        
+    },
+    {
+        id: 7,
+        date: "00-00-0000",
+        time: "00:00XX",
+        title: <div>HOOPLAH HOOOPLAY</div>,
+        image: "https://www.dropbox.com/s/u0a5lxj96oue7fa/IMG_1728.jpg?raw=1",
+        content: <div>
+            February 15th, 2022 arrives and I never smiled more. Getting the news of graduation was the most important moment of my life. 
+            </div>
+        
+    }
+]
+export default blogsData;
+
+
+
+
+
+
+
+
+
