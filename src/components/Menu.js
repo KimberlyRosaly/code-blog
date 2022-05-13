e87be476f7c33a61db62ccb5331184aa94dcb439
KimberlyRosaly <01010110.v@gmail.com>
2022-05-12 23:35:33 -0400

    blog titles have a click event handler inside Blog container component, accessible by props

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -15,19 +15,24 @@ const useStyles = makeStyles(theme => ({
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',      
 
+        // WHITE TEXT - BLACK  S T R O K E  EFFECT
         color: 'white',
         textShadow: '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000',
     }
 }))
 
-export default function Menu() {
-    // ENABLE CUSTOMIZATIONS OF STYLE ATTRIBUTES WITH MATERIAL UI
+export default function Menu(props) {
+    // ENABLE C U S T O M I Z A T I O N S OF STYLE ATTRIBUTES WITH MATERIAL UI
     const classes = useStyles()
 
+
+    // I T E R A T E  THROUGH ALL BLOG JSON DATA > R E N D E R  CLICKABLE TITLE BUTTONS > 
     return (
       <Grid container justifyContent='center' spacing={1}>
         {blogsData.map((b) => 
-            <Grid item xs="auto" key={b.id}  >
+
+        // THE GRID CHILD CARRIES A REQUIRED KEY - EASILY PASS THAT ID INTO EVENTHANDLING > WRITTEN IN PARENT, FUNCTION ACCESSIBLE VIA PROPS
+            <Grid item xs="auto" key={b.id}  onClick={(event) => props.handleClick(event, b.id)}>
                 <Paper 
                     classes={{root: classes.paperRoot}} 
                     elevation={4}
