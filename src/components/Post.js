c2d7c35e530697a86715c8c69ecab8b2ba9fecb2
KimberlyRosaly <01010110.v@gmail.com>
2022-05-13 23:23:23 -0400

    Presentational component returns a header and content on a card for an individual blog post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,11 @@
 import React from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-MATERIAL UI IMPORTS
-import { makeStyles, Paper } from '@material-ui/core'
+import { makeStyles, Paper, Typography } from '@material-ui/core'
+import { Box, Grid, Card, CardMedia, CardContent  } from '@material-ui/core'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
+// C U S T O M    S T Y L E  ATTRIBUTES
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 const useStyles = makeStyles(theme => ({
     paperRoot: {
@@ -15,15 +17,32 @@ const useStyles = makeStyles(theme => ({
 }))
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+// DISPLAY  S I N G L E   B L O G  POST DATA > HEADER & BODY
 export default function Post(props) {
-
+    // E N A B L E  USAGE OF S T Y L E   C U S T O M I Z A T I O N
     const classes = useStyles()
 
-    return (
+    return (<>
         
-            <Paper className={classes.paperRoot} elevation={4} >
-                {props.blog.content}
+            
+            <Paper className={classes.paperRoot} elevation={4} >              
+
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Grid container spacing={1}>
+
+                    <Paper><CardMedia><img style={{maxWidth:"100px"}} src={`${props.blog.image}`} /></CardMedia></Paper>
+                    <Typography>{props.blog.title}</Typography>
+                    <Typography>{props.blog.date}</Typography>
+                        
+                        </Grid>
+                    </Box>
+                    
+
+
+                    <Card>
+                    <CardContent><Typography>{props.blog.content}</Typography></CardContent>
+                    </Card>
             </Paper>          
 
-    )
+    </>)
 }
