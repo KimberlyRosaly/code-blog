1453db16b71069ee5701c66e761847455f0cde15
KimberlyRosaly <01010110.v@gmail.com>
2022-05-13 20:54:36 -0400

    use Paper component from Material UI and customize the style a bit - pass it in through classes

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,29 @@
 import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-MATERIAL UI IMPORTS
+import { makeStyles, Paper } from '@material-ui/core'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const useStyles = makeStyles(theme => ({
+    paperRoot: {
+        padding: '15px',
+        marginTop: '50px',
+        marginLeft: '100px',
+        marginRight: '100px',
+    }
+}))
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 export default function Post(props) {
+
+    const classes = useStyles()
+
     return (
-        <div>
-            POST COMPONENT LIVES HERE
+        
+            <Paper className={classes.paperRoot} elevation={4} >
+                {props.blog.content}
+            </Paper>          
 
-            {props.blog.content}
-
-        </div>
     )
 }
