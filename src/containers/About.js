a8721dae95cf33554b8b4d551e8e6070326cdb5f
KimberlyRosaly <01010110.v@gmail.com>
2022-05-15 01:05:44 -0400

    Toss in some relevant text to display my personal information with some styling passed in to Material UI, make use of flex / grid for easy centering in the layout

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,14 +1,22 @@
 import React from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-MATERIAL UI IMPORTS
-import { makeStyles, Paper} from '@material-ui/core'
+import { Grid, Typography, makeStyles, Paper} from '@material-ui/core'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
 // C U S T O M    S T Y L E  ATTRIBUTES
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
     paperRoot: {
-        
+        padding: '15px',
+        marginTop: '50px',
+        marginBottom: '50px',
+        marginLeft: '100px',
+        marginRight: '100px',
+
     }
 }))
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -20,7 +28,30 @@ export default function About() {
 
     return (<>
                 
-                <Paper>ABOUT ME LIVES HERE</Paper>
+                <Paper className={classes.paperRoot}>
+                    
+                
 
+                <Grid container
+                justifyContent='center'
+                alignItems='center'
+                spacing={1}>
+
+                    <Grid item xs={5}>
+                    <img 
+                    style={{maxWidth:"200px", borderRadius:"100%"}}
+                    src={`${'https://www.dropbox.com/s/q715ljny8yv5zl7/File_000.jpeg?raw=1'}`} />
+                    </Grid>
+
+                    <Grid item xs>
+                    <Paper className={classes.paper}>
+                    <Typography variant='h6'>Kimberly Rosaly</Typography>
+                    <Typography variant='h6'>Software Engineer</Typography>
+                    <Typography variant='h6'>Bay Shore, New York</Typography>
+                    </Paper>
+                    </Grid>
+
+                </Grid>
+                </Paper>
     </>)
 }
