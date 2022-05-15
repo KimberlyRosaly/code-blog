6bef6c8c337a0a92b96a5905568af83d2e550fff
KimberlyRosaly <01010110.v@gmail.com>
2022-05-15 05:25:12 -0400

    Box with Grid container and Grid items to help with alignment,  insert blank element for the DOM to hold a reference to and scroll smoothly to when the DOM updates

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect, useRef } from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-MATERIAL UI IMPORTS
 import { makeStyles, Paper, Typography } from '@material-ui/core'
 import { Box, Grid, Card, CardMedia, CardContent  } from '@material-ui/core'
@@ -17,10 +18,15 @@ const useStyles = makeStyles(theme => ({
 }))
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-// DISPLAY  S I N G L E   B L O G  POST DATA > HEADER & BODY
+// DISPLAY S I N G L E   B L O G  POST DATA > HEADER & BODY
 export default function Post(props) {
     // E N A B L E  USAGE OF S T Y L E   C U S T O M I Z A T I O N
     const classes = useStyles()
+    // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =S C R O L L   TO DUMMY DIV
+    const dummyDiv = useRef(null)
+    const scrollToBottom = () => { dummyDiv.current?.scrollIntoView({ behavior: "smooth" }) }
+    useEffect( () => {scrollToBottom()} )
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
     return (<>
         
@@ -28,21 +34,40 @@ export default function Post(props) {
             <Paper className={classes.paperRoot} elevation={4} >              
 
                     <Box sx={{ flexGrow: 1 }}>
-                        <Grid container spacing={1}>
+                        <Grid container
+                            spacing={3}
+                            direction="row"
+                            justifyContent='space-around'
+                            alignItems='center'>
+                            
+                            <Grid item >
+                                <Paper><img src={`${props.blog.image}`} style={{maxWidth: '33%'}} /></Paper>
+                            </Grid>
 
-                    <Paper><CardMedia><img style={{maxWidth:"100px"}} src={`${props.blog.image}`} /></CardMedia></Paper>
-                    <Typography>{props.blog.title}</Typography>
-                    <Typography>{props.blog.date}</Typography>
+                            <Grid item>
+                                <Typography>{props.blog.title}</Typography>
+                            </Grid>
+
+                            <Grid item >
+                                <Typography>{props.blog.date}</Typography>
+                            </Grid>
+
                         
                         </Grid>
                     </Box>
                     
-
+                    {/* =--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}                 
+                                                          
+                    {/* =--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */}
 
                     <Card>
-                    <CardContent><Typography>{props.blog.content}</Typography></CardContent>
+                    <CardContent><Typography>{props.blog.content}</Typography></CardContent> 
                     </Card>
-            </Paper>          
+            </Paper>     
+            
+                        {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */}
+                        <div ref={dummyDiv} />     
+                        {/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */}
 
     </>)
 }
