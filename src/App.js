ea70344766533cad841df824d0d7328fedc76630
KimberlyRosaly <01010110.v@gmail.com>
2022-05-23 23:36:45 -0400

    declare a nested route to contain a blogId, soorrrrta RESTful Routing.. may change

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ import NavBar from './components/NavBar'
 import Header from './components/Header'
 import Blog from './containers/Blog'
 import About from './containers/About'
+import Post from './components/Post'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+// GET BLOGID PARAM FROM THE URL
 
 function App() {
+  
   return (<>
 
     <NavBar />
@@ -18,7 +21,10 @@ function App() {
 
     <Routes>
       
-      <Route path="/blog" element={<Blog />} />
+      <Route path="/blog" element={<Blog />} >
+        <Route path=":blogId" element={<Post />} />
+      </Route>
+
       <Route path="/about" element={<About />} />
       
     </Routes>   
