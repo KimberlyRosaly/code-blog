ab21021ecbff91940064eb0884d8e79dc2277265
KimberlyRosaly <01010110.v@gmail.com>
2022-05-24 15:56:46 -0400

    finalize styling with Material UI and customization

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,24 +1,20 @@
 import React from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-MATERIAL UI IMPORTS
-import { ButtonGroup, Button, Grid, Typography, makeStyles, Paper} from '@material-ui/core'
-
+import { ButtonGroup, Button } from '@material-ui/core'
+import { Grid, Typography, Paper} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
 // C U S T O M    S T Y L E  ATTRIBUTES
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-const useStyles = makeStyles(theme => ({
-    
+const useStyles = makeStyles(theme => ({    
     paperRoot: {
         display: 'flex',
-        justifyContent: 'center',
-        padding: '15px',
-        marginTop: '50px',
-        marginBottom: '50px',
-        marginLeft: '100px',
-        marginRight: '100px',
-        
-
+        justifyContent: 'center',        
+        alignItems: 'center',        
+        //      TOP > RIGHT > BOTTOM > LEFT
+        padding: '30PX 70PX 30PX 70PX'      
     }
 }))
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -27,39 +23,37 @@ const useStyles = makeStyles(theme => ({
 export default function About() {
     // E N A B L E  USAGE OF S T Y L E   C U S T O M I Z A T I O N
     const classes = useStyles()
-
-    return (<>
-                
-                    
-                
-
+    
+    return (<>           
                 <Grid container
-                justifyContent='center'
-                alignItems='center'
-                spacing={0}>
-
+                // CENTER CONTAINER ON PAGE
+                justifyContent="center"
+                >
                 <Paper className={classes.paperRoot} elevation={15}>
 
+                    {/* // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - I M A G E */}
                     <Grid item xs>
                         <img 
                         alt='self portrait'
-                        style={{maxWidth:"200px", borderRadius:"5%"}}
-                        src={`${'https://www.dropbox.com/s/q715ljny8yv5zl7/File_000.jpeg?raw=1'}`} />
+                        style={{width:"300px", borderRadius:"5%"}}
+                        src={`${'https://www.dropbox.com/s/q715ljny8yv5zl7/File_000.jpeg?raw=1'}`} />                 
                     </Grid>
+                    {/* // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */}
+                    {/* // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - I N F O */}
+                    <Grid item style={{paddingLeft:"20px"}}>
+                        <Paper style={{padding:"20px"}}>
+                            <Typography variant='h5'>Kimberly Rosaly</Typography>
+                            <Typography variant='h6'>Software Engineer</Typography>
+                            <Typography variant='h6'>Bay Shore, New York</Typography>
 
-                    <Grid item xs>
-                    <Paper>
-                                <Typography variant='h5'>Kimberly Rosaly</Typography>
-                                <Typography variant='h6'>Software Engineer</Typography>
-                                <Typography variant='h6'>Bay Shore, New York</Typography>
 
-
-                                <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                                <Button variant="outlined" href='https://www.linkedin.com/in/kimberly-rosaly/ '>LinkedIn</Button>
-                                <Button variant="outlined" href='https://github.com/KimberlyRosaly/'>GitHub</Button>
-                                </ButtonGroup>
-                    </Paper>                  
+                            <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                            <Button variant="outlined" href='https://www.linkedin.com/in/kimberly-rosaly/ '>LinkedIn</Button>
+                            <Button variant="outlined" href='https://github.com/KimberlyRosaly/'>GitHub</Button>
+                            </ButtonGroup>
+                        </Paper>                  
                     </Grid>
+                    {/* // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */}
 
                 </Paper>
                 </Grid>
