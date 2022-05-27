fcd488acbe7de6ac0996bb6c0ceaa02de36fcd8c
KimberlyRosaly <01010110.v@gmail.com>
2022-05-26 21:04:28 -0400

    insert blog image

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,24 +2,39 @@ import React from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import { useParams } from 'react-router-dom'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import { Paper, Grid } from '@material-ui/core'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
 
 // DISPLAY S I N G L E   B L O G  POST DATA > HEADER & BODY
 export default function Post(props) {
     
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    //                                         P A R A M S  HANDLING
+    //! RETRIEVE URL PARAMS DEFINED IN APP.JS (I THINK..PLEASE LOOK UP)
     const urlParams = useParams()    
     let blogID = parseInt(urlParams.blogId)
     const blog = props.blogs.find( (b) => b.id == blogID )
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
     
     return (<>
-        
-       <div>{blog.title}</div>     
-       <div>{blog.date}</div>     
-       <div>{blog.content}</div>               
+        <Grid container 
+        justifyContent='center'
+        >
+        <Paper >
 
+        <img 
+              style={{maxWidth:"200px"}}
+              src={`${blog.image}`} />
+        <div>{blog.title}</div>     
+        <div>{blog.date}</div>     
+        <div>{blog.content}</div>               
+        </Paper></Grid>
 
     </>)
 }
