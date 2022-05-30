3791a7e76938711d8f4609bcad8dc385ee1f94ac
KimberlyRosaly <01010110.v@gmail.com>
2022-05-29 22:10:55 -0400

    hook up some testy conditional rendering with state

--- src/components/Guide.js
+++ src/components/Guide.js
@@ -1,7 +1,8 @@
 import React from 'react'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import { useState } from 'react'
-import { useEffect } from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import { Paper, Typography } from '@material-ui/core'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 // DESCRIBE TO USER WHERE THEY ARE & WHAT THEY CAN DO
@@ -10,15 +11,42 @@ export default function Guide() {
     // ==================================================
     //          MAKE A SAFE SPACE FOR  S T A T E  TO LIVE
     // PROPERTY KEY, FUNCTION TO CHANGE KEY'S VALUE = DEFAULT VALUE
-    const [value, setValue] = useState(false)
-    // ==================================================
-    //  AFTER 1ST RENDER & AFTER S T A T E   U P D A T E S
-    useEffect(() => {console.log("Hey, buddy. Nice to see you.")})
+    const [value, setValue] = useState(true)
     // ==================================================
 
 
-    // CONDITIONAL RENDERING : BASED ON URL
+    // ==================================================
+
+    // T O   D O 
+    // CONDITIONAL RENDERING 
+    //         : BASED ON URL
+    //           GRAB CONTENTS OF URL
+    // IF URL HAS BLOG:ID > BLOGPOST GUIDE
+    // IF URL HAS ABOUT > ABOUTME GUIDE
+
+
+    function whereTest2() {
+        return (
+            <Paper>
+            <Typography align='center'>CHECK OUT ALL ABOUT ME, ONE DAY</Typography>
+            </Paper>
+        )
+    }
+    function whereTest() {
+        return (
+            <Paper>
+            <Typography align='center'>hi there - guide here!</Typography>
+            </Paper>
+        )
+    }
+
+
     return (
-        <div>hi there - guide here!</div>
+        <>
+          { value ? whereTest() : whereTest2 }
+        </>
     )
-}
+        
+
+    
+    }
